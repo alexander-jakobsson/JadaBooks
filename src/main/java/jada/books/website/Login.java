9dad986db74578d4f14afdcebe370a446e11eb33
annataliveer <annataliveer@yahoo.se>
2017-11-21 16:59:25 +0100

    Merged login

--- src/main/java/jada/books/website/Login.java
+++ src/main/java/jada/books/website/Login.java
@@ -1,14 +1,16 @@
 package jada.books.website;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Login {
 
-    @RequestMapping("login")
-    String login() {
-        return "Log in here.";
+    @PostMapping("/login")
+    String login(@RequestParam String user, @RequestParam String pw) {
+        return user;
     }
 
 }
