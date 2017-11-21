9b4722856900009a41e3c60cea2e947752460cae
Alexander Jakobsson <jakobsson.alex@gmail.com>
2017-11-21 11:44:00 +0100

    Add initial files.

---
+++ src/main/java/jada/books/website/Index.java
@@ -0,0 +1,14 @@
+package jada.books.website;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Index {
+
+    @RequestMapping("/")
+    String index() {
+        return "Hello Academy!";
+    }
+
+}
