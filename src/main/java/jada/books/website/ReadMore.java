a47b72adfcf510eec77f0d0535680a10564a7a53
Dafvid Skoogh <dafvid.skoogh@gmail.com>
2017-11-22 12:43:43 +0100

    Add ReadMore feature

---
+++ src/main/java/jada/books/website/ReadMore.java
@@ -0,0 +1,12 @@
+package jada.books.website;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class ReadMore {
+
+
+    @GetMapping("/readmore/{buttonID}")
+    public int readMore() {
+        return 
+    }
+}
--- src/main/resources/static/code.js
+++ src/main/resources/static/code.js
@@ -21,26 +21,69 @@ function bookSearch() {
         dataType: "json",
 
         success: function(data) {
+            //Array[data.items.length] arr = new Array[];
+
             for (i = 0; i < data.items.length; i++ ) {
+                var image = data.items[i].volumeInfo.imageLinks.smallThumbnail;
+                var title = data.items[i].volumeInfo.title;
+                var author = data.items[i].volumeInfo.authors;
+                var idType = data.items[i].volumeInfo.industryIdentifiers[0].type;
+
+                if (idType === "ISBN_10") {
+                    var bookID = data.items[i].volumeInfo.industryIdentifiers[0].identifier;
+                } else if (idType === "ISBN_13") {
+                    var bookID = data.items[i].volumeInfo.industryIdentifiers[1].identifier;
+                }
+
                 if (data.items[i].volumeInfo.description) {
                     var parag = data.items[i].volumeInfo.description;
                     parag = truncate(parag);
-                    results.innerHTML += "<div class=\"box\"> <img src=\"" + data.items[i].volumeInfo.imageLinks.smallThumbnail
-                    + "\" id=\"resultImg\"> <h3>" + data.items[i].volumeInfo.title
-                    + "</h3> <h5>" + data.items[i].volumeInfo.authors
-                    + "</h5> <p>" + parag
-                    + "</p> <div> <button id=\"b" + i + "\">Read More</button> </div> </div>";
                 } else {
-                    results.innerHTML += "<div class=\"box\"> <img src=\"" + data.items[i].volumeInfo.imageLinks.smallThumbnail
-                    + "\" id=\"resultImg\"> <h3>" + data.items[i].volumeInfo.title
-                    + "</h3> <h5>" + data.items[i].volumeInfo.authors
-                    + "</h5> <div> <button id=\"b" + i + "\">Read More</button> </div> </div>";
+                    var parag = "";
                 }
+                results.innerHTML += "<div class=\"box\"> <img src=\"" + image
+                + "\" id=\"resultImg\"> <h3>" + title
+                + "</h3> <h5>" + author
+                + "</h5> <p>" + parag
+                + "</p> <div> <button class=\"readMoreBtn\" id=" + bookID + " onClick=\"reply_click(this.id)\">Read More</button> </div> </div>";
+                console.log(i);
             }
             if (search) {
                 document.getElementById("notaMember").style.display = "none";
             }
             console.log(search);
+        },
+
+        type: 'GET'
+    })
+}
+
+//document.getElementsByClassName("readMoreBtn").addEventListener("click", reply_click(clicked_id));
+
+function reply_click(clicked_id) {
+    document.getElementById("readMore").innerHTML = "";
+    alert(clicked_id);
+
+    $.ajax({
+        url: "https://www.googleapis.com/books/v1/volumes?q=isbn:" + clicked_id,
+        dataType: "json",
+
+        success: function(data) {
+
+            readMore.innerHTML = "<p>" + clicked_id + "</p>"
+            console.log(data);
+            var image = data.items[0].volumeInfo.imageLinks.smallThumbnail;
+            var title = data.items[0].volumeInfo.title;
+            var author = data.items[0].volumeInfo.authors;
+            var idType = data.items[0].volumeInfo.industryIdentifiers[0].type;
+            var parag = data.items[0].volumeInfo.description;
+
+            readMore.innerHTML += "<div class=\"aboutBox\"> <img src=\"" + image
+            + "\" id=\"resultImg\"> <h3>" + title
+            + "</h3> <h5>" + author
+            + "</h5> <p>" + parag
+            + "</p> </div>";
+
         },
 
         type: 'GET'
