fd8b1444860c604cfc802d6eed7c309c6cc89b56
Dafvid Skoogh <dafvid.skoogh@gmail.com>
2017-11-23 13:03:52 +0100

    Add member registration

---
+++ src/main/java/jada/books/website/RegMember.java
@@ -0,0 +1,32 @@
+package jada.books.website;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+@Controller
+public class RegMember {
+    @Autowired
+    private DataSource dataSource;
+
+    @PostMapping("/regMem")
+    public String postMem(String inputFname, String inputLname, String inputAddress,
+                        String inputCity, String inputEmail, String inputPass) throws SQLException {
+        try (Connection conn = dataSource.getConnection();
+            PreparedStatement ps = conn.prepareStatement("INSERT INTO DBO.Users (FirstName,LastName,Address,City,Email,Password)\n" +
+                     "VALUES (?,?,?,?,?,?) ", new String[] {"UserID"})) {
+            ps.setString(1, inputFname);
+            ps.setString(2, inputLname);
+            ps.setString(3, inputAddress);
+            ps.setString(4, inputCity);
+            ps.setString(5, inputEmail);
+            ps.setString(6, inputPass);
+            ps.executeUpdate();
+        } return "redirect:/validation.html";
+    }
+}
---
+++ src/main/java/jada/books/website/Users.java
@@ -0,0 +1,74 @@
+package jada.books.website;
+
+public class Users {
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String city;
+    private String email;
+    private String password;
+
+    public Users(String firstName, String lastName, String address, String city, String email, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.email = email;
+        this.password = password;
+    }
+
+    public Users(String firstName, String lastName, String email, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
