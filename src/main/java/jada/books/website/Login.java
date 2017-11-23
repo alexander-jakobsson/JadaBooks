5795e0a4c6118e40a5d63b9b083aea9ab9b0e999
Alexander Jakobsson <jakobsson.alex@gmail.com>
2017-11-23 15:38:46 +0100

    Re-add Login.java

---
+++ src/main/java/jada/books/website/Login.java
@@ -0,0 +1,38 @@
+package jada.books.website;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+@Controller
+public class Login {
+
+    @Autowired
+    private Repository repository;
+    private DataSource dataSource;
+
+    @PostMapping("/login")
+    //String login(@RequestParam String user, @RequestParam String pw) {
+    ModelAndView login(@RequestParam String user, @RequestParam String pw) {
+        Users userObj = repository.loginUser(user, pw);
+        if (userObj != null) {
+
+            // gör anrop till databasen för att få användaren (userObject)
+            // return new ModelAndView("login").addObject("user",userObject); // i html-koden heter userObject bara user
+
+            //return "login";
+            return new ModelAndView("login").addObject("user",userObj);
+        }
+        //return "redirect:/";
+        return new ModelAndView("redirect:/");
+    }
+}
